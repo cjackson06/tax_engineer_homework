3002e4c4a51cbda9066df5b7dd1f78d0d740986a
cjackson06 <114544866+cjackson06@users.noreply.github.com>
2025-06-05 19:07:50 -0400

    Feature: player schema

---
+++ schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class Player(BaseModel):
+    hand: list[str]
+    discard: list[str] = []
+
+    @property
+    def no_cards(self):
+        return len(self.hand) + len(self.discard) == 0
+
+    def refill_hand(self):
+        if len(self.hand) > 0:
+            return None
+        self.discard.reverse()
+        self.hand = self.discard.copy()
+        self.discard.clear()
+        return None
